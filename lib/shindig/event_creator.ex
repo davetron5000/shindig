448d90f909d5275634745a9126dcf68aa5ab1135
Dave Copeland <davec@naildrivin5.com>
2013-08-15 08:43:37 -0400

    ignore unused variables

--- lib/shindig/event_creator.ex
+++ lib/shindig/event_creator.ex
@@ -3,7 +3,7 @@ defmodule Shindig.EventCreator do
   def new_event(event_name,count), do: new_event(event_name,count,:erlang.now())
 
   @doc "Create a new event with the given name, count, and date in the form as returned by :erlang.now()"
-  def new_event(event_name,count,date = { mega,sec,micro}) do
+  def new_event(event_name,count,date = { _mega,_sec,_micro}) do
     Event.new name: event_name, count: count, timestamp: date
   end
 end
