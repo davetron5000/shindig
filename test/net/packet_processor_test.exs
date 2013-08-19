3a2a2ddc1e657a284c3ce335c9a5ada61192e748
Dave Copeland <davec@naildrivin5.com>
2013-08-19 09:27:18 -0400

    validate usage of this

--- test/net/packet_processor_test.exs
+++ test/net/packet_processor_test.exs
@@ -3,4 +3,26 @@ defmodule Shinding.Test.Net.PacketProcessor do
   import Shindig.Net.PacketProcessor
   doctest Shindig.Net.PacketProcessor
 
+  test "can turn a chunky stream into events" do
+    chunks = [ "foo 99",
+               "0 10000200\n",
+               "blah 10 100",
+               "00210\n" ]
+
+    event_creator = &Shindig.EventCreator.new_event_from_plaintext/1
+    {all_events, unparsed} = Enum.reduce(
+      chunks,
+      {[],""},
+      fn(chunk,{events,unparsed}) ->
+        {:ok, parsed_events, rest } = process_packet(unparsed <> chunk,event_creator)
+        { events ++ parsed_events,rest }
+      end)
+
+    assert all_events == [
+      {:ok, Shindig.Event[name: "foo",  count: 990, timestamp: { 10, 200, 0 }]},
+      {:ok, Shindig.Event[name: "blah", count: 10,  timestamp: { 10, 210, 0 }]}
+    ]
+    assert unparsed == ""
+  end
+
 end
